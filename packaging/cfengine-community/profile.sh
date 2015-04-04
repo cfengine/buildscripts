8dee45f09c7569e814f68386a26934a1aa104fe0
Luka Furlan <luka@evriala.net>
2015-04-04 12:18:11 +0100

    Check PATH and MANPATH content before expanding them.

--- packaging/cfengine-community/profile.sh
+++ packaging/cfengine-community/profile.sh
@@ -1,2 +1,7 @@
-export PATH=$PATH:/var/cfengine/bin
-export MANPATH=$MANPATH:/var/cfengine/share/man
+if ! echo ${PATH} | /bin/grep -q /var/cfengine/bin ; then
+    export PATH=$PATH:/var/cfengine/bin
+fi
+
+if ! echo ${MANPATH} | /bin/grep -q /var/cfengine/share/man ; then
+    export MANPATH=$MANPATH:/var/cfengine/share/man
+fi
