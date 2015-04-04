24a8195a1166ae32f279ccb19c423ad87234447f
Luka Furlan <luka@evriala.net>
2015-04-04 12:18:11 +0100

    Check PATH and MANPATH content before expanding them.
    Modified by: Kristian Amlie <kristian.amlie@cfengine.com>
    - Replaced '-q' with redirection. '-q' is not POSIX.

--- packaging/cfengine-community/profile.sh
+++ packaging/cfengine-community/profile.sh
@@ -1,2 +1,7 @@
-export PATH=$PATH:/var/cfengine/bin
-export MANPATH=$MANPATH:/var/cfengine/share/man
+if ! echo ${PATH} | /bin/grep /var/cfengine/bin > /dev/null ; then
+    export PATH=$PATH:/var/cfengine/bin
+fi
+
+if ! echo ${MANPATH} | /bin/grep /var/cfengine/share/man > /dev/null ; then
+    export MANPATH=$MANPATH:/var/cfengine/share/man
+fi
