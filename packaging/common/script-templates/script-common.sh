4347d7eac0007e72bed2f87c0ad7e0fb480ba5b2
Kristian Amlie <kristian.amlie@cfengine.com>
2015-06-15 13:14:42 +0200

    Fix incorrect upgrade detection in install removal scripts.
    We used to not even try to detect upgrades in the removal scripts,
    since we assume we will not run removal scripts then, but we did not
    take into account that we need to detect those upgrades exactly to
    avoid calling the removal scripts (see "We want to skip removal scripts
    on upgrade" comment in the scripts).

---
+++ packaging/common/script-templates/script-common.sh
@@ -0,0 +1,28 @@
+# Upgrade detection is a mess. It is often difficult to tell, especially from
+# the postinstall script, so we use the package-upgrade.txt file to remember.
+
+case "$PKG_TYPE" in
+  depot|deb|bff)
+    case "$SCRIPT_TYPE" in
+      preinstall|preremove)
+        if native_is_upgrade; then
+          mkdir -p "$PREFIX"
+          echo "File used by CFEngine during package upgrade. Can be safely deleted." > "$PREFIX/package-upgrade.txt"
+        else
+          rm -f "$PREFIX/package-upgrade.txt"
+        fi
+        alias is_upgrade='native_is_upgrade'
+        ;;
+      postremove|postinstall)
+        if [ -f "$PREFIX/package-upgrade.txt" ]; then
+          if "$SCRIPT_TYPE" = "postinstall"; then
+            rm -f "$PREFIX/package-upgrade.txt"
+          fi
+          alias is_upgrade='true'
+        else
+          alias is_upgrade='false'
+        fi
+        ;;
+    esac
+    ;;
+esac
