34e0b9526b18a37d89c3e729faf42f529e7b57d0
Kristian Amlie <kristian.amlie@cfengine.com>
2015-08-28 12:09:48 +0200

    Redmine #7193: Fix an error in logic when deciding to run removal script
    Upgrade is not detected correctly with bff and depot packages when
    removing the package, because they use the existence of the binaries
    to detect this. Obviously the binaries will exist when removing a
    package, but this doesn't mean it's an upgrade. However, they don't
    need this detection, because they don't run the removal scripts on
    upgrade, so we can avoid this logic altogether.
    
    Changelog: Fixed bug which would cause bff and depot packages not to
    run package scripts on removal.
    (cherry picked from commit 6924bf0cdb43f909b4c26ff1b6bc7b11c12b12a1)

--- packaging/common/script-templates/script-common-remove.sh
+++ packaging/common/script-templates/script-common-remove.sh
@@ -1,4 +1,11 @@
-if is_upgrade; then
-  # We want to skip removal scripts on upgrade.
-  exit 0
-fi
+case `package_type` in
+  rpm|deb)
+    if is_upgrade; then
+      # We want to skip removal scripts on upgrade.
+      # However, non-rpm and non-deb package systems don't run removal scripts
+      # on upgrade, so don't do it there (they would not detect upgrade
+      # correctly in this situation anyway).
+      exit 0
+    fi
+    ;;
+esac
