762cf2701ba4d871449f0f33b64e0878e251054f
Kristian Amlie <kristian.amlie@cfengine.com>
2015-09-04 11:06:51 +0200

    Fix upgrade causing error message under systemd because of open ports.
    It happened because the shutdown of the web services was delayed and
    the script checked the ports immediately.
    
    Changelog: Title

--- packaging/common/cfengine-hub/preremove.sh
+++ packaging/common/cfengine-hub/preremove.sh
@@ -1,4 +1,11 @@
 platform_service cfengine3 stop
+if [ -x /bin/systemctl ]; then
+  # When using systemd, the services are split in two, and although both will
+  # stop due to the command above, the web part may only do so after some
+  # delay, which may cause problems later if the binaries are gone by the time
+  # it tries to stop them.
+  /bin/systemctl stop cfengine3-web
+fi
 
 case "`os_type`" in
   redhat)
