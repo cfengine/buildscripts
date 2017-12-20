7effca289bf684d993e1968a62295cf2dc56e521
aleksandrychev <leopolis.best@gmail.com>
2017-12-20 12:01:11 +0100

    cfdb_import.sql entity was moved to cfdb schema

--- packaging/common/cfengine-hub/postinstall.sh
+++ packaging/common/cfengine-hub/postinstall.sh
@@ -402,9 +402,6 @@ else
   (cd /tmp && chown cfpostgres "$PREFIX/share/GUI/phpcfenginenova/pgschema.sql" && su cfpostgres -c "$PREFIX/bin/psql cfmp -f $PREFIX/share/GUI/phpcfenginenova/pgschema.sql" && chown root "$PREFIX/share/GUI/phpcfenginenova/pgschema.sql")
   (cd /tmp && chown cfpostgres "$PREFIX/share/GUI/phpcfenginenova/ootb_import.sql" && su cfpostgres -c "$PREFIX/bin/psql cfmp -f $PREFIX/share/GUI/phpcfenginenova/ootb_import.sql" && chown root "$PREFIX/share/GUI/phpcfenginenova/ootb_import.sql")
 
-  #import stored function for MP into cfdb
-  (cd /tmp && chown cfpostgres "$PREFIX/share/GUI/phpcfenginenova/cfdb_import.sql" && su cfpostgres -c "$PREFIX/bin/psql cfdb -f $PREFIX/share/GUI/phpcfenginenova/cfdb_import.sql" && chown root "$PREFIX/share/GUI/phpcfenginenova/cfdb_import.sql")
-
 
   #create database for hub internal data
   (cd /tmp && chown cfpostgres "$PREFIX/share/db/schema_settings.sql" && su cfpostgres -c "$PREFIX/bin/psql cfsettings -f $PREFIX/share/db/schema_settings.sql" && chown root "$PREFIX/share/db/schema_settings.sql")
