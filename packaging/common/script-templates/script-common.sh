a89e658568c8bb7c33dd4b6a33a2c6db9ba6bc99
Vratislav Podzimek <v.podzimek@mykolab.com>
2019-01-10 14:14:52 +0100

    Only start services that still exist on state restore
    Some services may be gone after upgrade (e.g. cf-consumer).
    
    Ticket: ENT-3385
    Changelog: None

--- packaging/common/script-templates/script-common.sh
+++ packaging/common/script-templates/script-common.sh
@@ -53,7 +53,14 @@ restore_cfengine_state() {
     # $1 -- file where the state to restore is saved (see get_cfengine_state())
 
     if type systemctl >/dev/null 2>&1; then
-        xargs -n1 -a "$1" systemctl start
+        for service in `cat "$1"`; do
+            definition=`systemctl cat "$service"` || continue
+            # only try to start service that are defined/exist (some may be gone
+            # in the new version)
+            if [ -n "$definition" ]; then
+                systemctl start "$service"
+            fi
+        done
     else
         CALLED_FROM_STATE_RESTORE=1
         if [ -f ${PREFIX}/bin/cfengine3-nova-hub-init-d.sh ]; then
