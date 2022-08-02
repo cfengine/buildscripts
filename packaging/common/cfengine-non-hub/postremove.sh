1a1d3c0b3c66b0ad98347788bee944586407e70b
Craig Comstock <craig.comstock@northern.tech>
2022-08-02 14:06:13 -0500

    Added cf-support binary to packages
    Also includes cf-support-nova-hub script which is sourced
    by cf-support if present.
    
    Ticket: ENT-9037
    Changelog: title
    (cherry picked from commit 7072e83df827b0bd7c209783a315daf0bc61e128)
    
     Conflicts:
    	packaging/cfengine-nova-hub/cfengine-nova-hub.spec.in
    
    3.18 does not include cfbs and python so had to remove that part of the commit

--- packaging/common/cfengine-non-hub/postremove.sh
+++ packaging/common/cfengine-non-hub/postremove.sh
@@ -10,7 +10,7 @@ esac
 if [ -d /usr/local/sbin ]; then
   rm -f /usr/local/sbin/cf-agent /usr/local/sbin/cf-check /usr/local/sbin/cf-execd \
     /usr/local/sbin/cf-key /usr/local/sbin/cf-secret /usr/local/sbin/cf-know /usr/local/sbin/cf-monitord \
-    /usr/local/sbin/cf-net \
+    /usr/local/sbin/cf-net /usr/local/sbin/cf-support \
     /usr/local/sbin/cf-promises /usr/local/sbin/cf-report /usr/local/sbin/cf-runagent \
     /usr/local/sbin/cf-serverd /usr/local/sbin/cf-twin /usr/local/sbin/cf-hub /usr/local/sbin/cf-reactor > /dev/null 2>&1
 fi
