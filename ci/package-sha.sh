3fc0250c0d13861f9cd39df0de210a5dce6b0a22
Craig Comstock <craig.comstock@northern.tech>
2023-09-30 07:33:38 -0500

    Fixed ci/package-sha.sh
    Ticket: none
    Changelog: none
    (cherry picked from commit 5f40765d2aa6997ef710a239dbfac1da4eb3cbb6)

--- ci/package-sha.sh
+++ ci/package-sha.sh
@@ -15,9 +15,9 @@ echo "NOVA_SHA: ${NOVA_SHA}" >&2
 MASTERFILES_SHA=$(git -C "${NTECH_ROOT}/masterfiles" log --pretty='format:%h' -1 -- .)
 echo "MASTERFILES_SHA: ${MASTERFILES_SHA}" >&2
 # notice below the sha is more complex, a todo is to make "code only" shas for each repo ENT-10443
-MISSION_PORTAL_SHA=$(find "${NTECH_ROOT}/mission-portal" -type f -and \( -path "./application/*" -or -path "./public/*" -or -path "./phpcfenginenova/*" -or -path "./ldap/*" -or -path "./composer.json" -or -path "./package.json" -or -path "./static/*" \)  -print0 | xargs -0 sha1sum | awk '{print $1}' | sha1sum | cut -c -8)
+MISSION_PORTAL_SHA=$(find "${NTECH_ROOT}/mission-portal" -type f -and \( -path "*/mission-portal/application/*" -or -path "*/mission-portal/public/*" -or -path "*/mission-portal/phpcfenginenova/*" -or -path "*/mission-portal/ldap/*" -or -path "*/mission-portal/composer.json" -or -path "*/mission-portal/package.json" -or -path "*/mission-portal/static/*" \)  -print0 | xargs -0 sha1sum | awk '{print $1}' | sha1sum | cut -c -8)
 echo "MISSION_PORTAL_SHA: ${MISSION_PORTAL_SHA}" >&2
-BUILDSCRIPTS_SHA=$(find "${NTECH_ROOT}/buildscripts" -type f -and \( -path "./deps-packaging/*" -or -path "./build-scripts/*" -or -path "./packaging/*" \) -print0 | xargs -0 sha1sum | awk '{print $1}' | sha1sum | cut -c -8)
+BUILDSCRIPTS_SHA=$(find "${NTECH_ROOT}/buildscripts" -type f -and \( -path "*/deps-packaging/*"-or -path "*/build-scripts/*" -or -path "*/packaging/*" \) -print0 | xargs -0 sha1sum | awk '{print $1}' | sha1sum | cut -c -8)
 echo "BUILDSCRIPTS_SHA: ${BUILDSCRIPTS_SHA}" >&2
 
 PACKAGE_SHA=$(echo "$CORE_SHA" "$ENTERPRISE_SHA" "$NOVA_SHA" "$MASTERFILES_SHA" "$MISSION_PORTAL_SHA" "$BUILDSCRIPTS_SHA" | sha256sum | cut -d' ' -f1 | cut -c -8)
