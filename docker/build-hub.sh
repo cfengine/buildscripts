c2ce06127aa70e089de997ad199b63d41e45935d
Craig Comstock <craig.comstock@northern.tech>
2025-02-12 11:04:20 -0600

    fixup client

--- docker/build-hub.sh
+++ docker/build-hub.sh
@@ -24,16 +24,3 @@ docker exec -it hub bash
 
 # and now... make the final image and publish to hub.docker.com, free25 in a container! :)
 docker commit hub cfengine-hub:main
-
-cf-remote --version master download debian12 client amd64
-client_pkg=$(ls -tr "$PKG_DIR" | grep cfengine-nova
-cp "$PKG_DIR"/"$client_pkg" client.deb
-
-docker stop client || true
-docker rm client || true
-docker rmi cfengine-client:main || true
-docker build --tag cfengine-client:main -f Dockerfile-client .
-docker run -h client -d --name client cfengine-client:main sleep infinity
-# for developer, we run hub listening on local 5308 HOST port and so bootstrap to HOST IP address, hostname -i on the host, not in the container.
-docker exec client cf-agent -IB $(hostname -i)
-docker commit client cfengine-client:main
