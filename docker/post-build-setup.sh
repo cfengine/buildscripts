db34a6240a75baccd0033077e3aad1a772998311
Craig Comstock <craig.comstock@northern.tech>
2025-02-24 15:19:01 -0600

    restore post-build-setup script items for docker case

--- docker/post-build-setup.sh
+++ docker/post-build-setup.sh
@@ -1,10 +1,10 @@
-#dpkg --configure cfengine-nova-hub
+dpkg --configure cfengine-nova-hub
 # to reduce image size, let's lop off share/GUI, it is not technically required and duplicates over 700MB of node_modules in public/scripts
-#rm -rf /var/cfengine/share/GUI
-#cf-agent -IB $(hostname -i)
-#cf-agent -KIf update.cf
-#cf-agent -KI
-#cf-agent -KI
-#cf-agent -KI
+rm -rf /var/cfengine/share/GUI
+cf-agent -IB $(hostname -i)
+cf-agent -KIf update.cf
+cf-agent -KI
+cf-agent -KI
+cf-agent -KI
 # running cfengine3 init script is not needed after bootstrapping, bootstrapping starts all needed services
 #/etc/init.d/cfengine3 start
