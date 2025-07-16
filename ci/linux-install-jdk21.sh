d1137d650465263685f839d6c56d4fa02f4c0aec
Lars Erik Wik <lars.erik.wik@northern.tech>
2025-07-16 16:39:54 +0200

    linux-install-jdk21.sh: Use archive URL
    Signed-off-by: Lars Erik Wik <lars.erik.wik@northern.tech>

--- ci/linux-install-jdk21.sh
+++ ci/linux-install-jdk21.sh
@@ -5,14 +5,14 @@ install_jdk() {
   # install jdk "manually"
   # depending on os, might want to do something like `apt remove default-jre openjdk-*-jre-*`
   cd /opt
-  baseurl=https://download.oracle.com/java/21/latest/
+  baseurl=https://download.oracle.com/java/21/archive/
   version=21.0.8
   if uname -m | grep aarch64; then
-    tarball=jdk-21_linux-aarch64_bin.tar.gz
+    tarball=jdk-$(version)_linux-aarch64_bin.tar.gz
     # checksum from https://download.oracle.com/java/21/latest/jdk-21_linux-aarch64_bin.tar.gz.sha256
     sha=708064ee3a1844245d83be483ff42cc9ca0c482886a98be7f889dff69ac77850
   else
-    tarball=jdk-21_linux-x64_bin.tar.gz
+    tarball=jdk-$(version)_linux-x64_bin.tar.gz
     # checksum from https://download.oracle.com/java/24/latest/jdk-24_linux-x64_bin.tar.gz.sha256
     sha=5f9f7c4ca2a6cef0f18a27465e1be81bddd8653218f450a329a2afc9bf2a1dd8
   fi
