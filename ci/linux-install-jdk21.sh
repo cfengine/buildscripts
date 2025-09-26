5a42f35a8ab71f0f1c38e8a54687369fd1b60e0a
Craig Comstock <craig.comstock@northern.tech>
2025-09-26 15:19:43 -0500

    Revert "Revert "Updated Java Development Kit to 21.0.8""
    This reverts commit fdec8fa7c6d7409a660713ab9d84da2d05896000.
    
    Working backwards to a working setup related to ENT-13247 changes.

--- ci/linux-install-jdk21.sh
+++ ci/linux-install-jdk21.sh
@@ -9,15 +9,15 @@ install_jdk() {
   baseurl=https://download.oracle.com/java/21/archive/
   major_version=21
   baseurl="https://download.oracle.com/java/${major_version}/archive/"
-  version=21.0.7
+  version=21.0.8
   if uname -m | grep aarch64; then
     tarball=jdk-${version}_linux-aarch64_bin.tar.gz
     # checksum from https://download.oracle.com/java/${major_version}/archive/jdk-${version}_linux-aarch64_bin.tar.gz.sha256
-    sha=47372cfa9244dc74ec783a1b287381502419b564fbd0b18abc8f2d6b19ac865e
+    sha=708064ee3a1844245d83be483ff42cc9ca0c482886a98be7f889dff69ac77850
   else
     tarball=jdk-${version}_linux-x64_bin.tar.gz
     # checksum from https://download.oracle.com/java/${major_version}/latest/jdk-${version}_linux-x64_bin.tar.gz.sha256
-    sha=267b10b14b4e5fada19aca3be3b961ce4f81f1bd3ffcd070e90a5586106125eb
+    sha=d87272944278713fc7a120cf024d2818d136b5debc750aa17045e3c6f045b867
   fi
   wget --quiet "$baseurl$tarball"
   echo "$sha  $tarball" | sha256sum --check -
