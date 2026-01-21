b206d6fc5bb0c7ab3be7458f43fc25e8afb213d4
Craig Comstock <craig.comstock@northern.tech>
2026-01-21 15:08:41 -0600

    Adjusted build host setup script for centos-6, no pip, no cf-remote
    Ticket: ENT-13483
    Changelog: none

--- ci/setup-cfengine-build-host.sh
+++ ci/setup-cfengine-build-host.sh
@@ -138,32 +138,36 @@ if grep suse /etc/os-release; then
   fi
 fi
 
-echo "Installing cf-remote for possible package install and masterfiles download"
-# try pipx first for debian as pip won't work.
-# If that fails to install CFEngine then try python3-pip for redhats.
-PIP=""
-software python3-venv || true # on ubuntu-20 this is needed, debian-12 it is not but won't hurt
-if software pipx; then
-  PIP=pipx
-  export PATH=$HOME/.local/bin:$PATH
-elif software python3-pip; then
-  if command -v pip; then
-    PIP=pip
-  elif command -v pip3; then
-    PIP=pip3
-  fi
-elif software python-pip; then
-  if command -v pip; then
-    PIP=pip
-  fi
-else
-  echo "Tried installing pipx, python3-pip and python-pip, none of which resulted in pipx, pip3 or pip being available. Exiting."
-  exit 23
-fi
-export PATH=/usr/local/bin:$PATH # some pip/pipx use /usr/local/bin
+if [ ! -x /var/cfengine/cf-agent ]; then
+  if ! ls cfengine-masterfiles*tar.gz; then
+    echo "Installing cf-remote for possible package install and masterfiles download"
+    # try pipx first for debian as pip won't work.
+    # If that fails to install CFEngine then try python3-pip for redhats.
+    PIP=""
+    software python3-venv || true # on ubuntu-20 this is needed, debian-12 it is not but won't hurt
+    if software pipx; then
+      PIP=pipx
+      export PATH=$HOME/.local/bin:$PATH
+    elif software python3-pip; then
+      if command -v pip; then
+        PIP=pip
+      elif command -v pip3; then
+        PIP=pip3
+      fi
+    elif software python-pip; then
+      if command -v pip; then
+        PIP=pip
+      fi
+    else
+      echo "Tried installing pipx, python3-pip and python-pip, none of which resulted in pipx, pip3 or pip being available. Exiting."
+      exit 23
+    fi
+    export PATH=/usr/local/bin:$PATH # some pip/pipx use /usr/local/bin
 
-$PIP uninstall -y cf-remote || true # just in case a previous is there and would cause the install to fail
-$PIP install cf-remote || true # if this fails we will try to install from source
+    $PIP uninstall -y cf-remote || true # just in case a previous is there and would cause the install to fail
+    $PIP install cf-remote || true # if this fails we will try to install from source
+  fi # no masterfiles downloaded
+fi # no cf-agent installed
 
 echo "Checking for pre-installed CFEngine (chicken/egg problem)"
 # We need a cf-agent to run build host setup policy and redhat-10-arm did not have a previous package to install.
