be57fc627d015ee80e70d879b4e0e3fbc9786ab6
Craig Comstock <craig.comstock@northern.tech>
2026-04-16 08:20:02 -0500

    Fixed build issues due to update of openssl from 3.6.1 to 4.0.0
    fix: remove openssl patch for mingw that was applied to 3.6.2
    The patch was applied to 3.6.2 at https://github.com/openssl/openssl/commit/a7b47bda72465ece33a70382d8da08a47e3b64aa
    
    fix: openssl 4.0.0 on centos-7 compilation problems
    fix: don't need deps-packaging/openssl/0010-Provide-timespec-for-centos-7.patch
    fix: packaging in windows requires change from version 3 to 4 and new GUIDs for libcrypto and libssl dlls
    
    Ticket: none
    Changelog: none

--- ci/fix-buildhost.sh
+++ ci/fix-buildhost.sh
@@ -1,3 +1,16 @@
+#!/usr/bin/env bash
+# it is expected that this file is sourced, not executed directly
+set -ex
+my_path="$(realpath "${BASH_SOURCE[0]}")"
+my_dir="$(dirname "$my_path")"
+
+if [ -f /etc/os-release ]; then
+  source /etc/os-release
+  if [ "$ID" = "centos" ] && [ "$VERSION_ID" = "7" ]; then
+    source "$my_dir"/centos-7-setup-devtoolset-11.sh
+  fi
+fi
+
 if [ "$(uname)" = "HP-UX" ]; then
   # /etc/profile contains tty code that won't work well when sourced and this VUE env var guards against running those bits
   # https://ftp.mirrorservice.org/sites/www.bitsavers.org/pdf/hp/9000_hpux/9.x/B1171-90044_HP_Visual_User_Environment_System_Administration_Manual_Nov91.pdf
@@ -11,10 +24,5 @@ if [ -f /etc/profile ]; then
   . /etc/profile
 fi
 
-# while ENT-13750 is in progress we need to ensure that OTHER builds include openssl devel packages on redhat-based platforms
-if command -v zypper >/dev/null 2>/dev/null; then
-  sudo zypper install -y libopenssl-devel || true
-fi
-if command -v yum >/dev/null 2>/dev/null; then
-  sudo yum install -y openssl-devel || true
-fi
+mkdir -p ~/.ssh
+echo "build-artifacts-cache.cloud.cfengine.com ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGahpsY8Phk2+isBmuJQjjQVlh6BNL/Qetc14g26gowV" >> ~/.ssh/known_hosts
