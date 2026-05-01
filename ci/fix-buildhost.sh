56f328fabbe25b0e70bd7fd19a859d9b8c9bf2f9
Craig Comstock <craig.comstock@northern.tech>
2026-05-01 13:04:40 -0500

    fix: add ci/fix-buildhost.sh to source /etc/profile especially for exotics which may have basic tools like ssh in odd places added to PATH in /etc/profile
    Ticket: ENT-14014
    Changelog: none
    (cherry picked from commit a7aad11e7b72bdc5159359b0c49970e41d42e382)

---
+++ ci/fix-buildhost.sh
@@ -0,0 +1,12 @@
+if [ "$(uname)" = "HP-UX" ]; then
+  # /etc/profile contains tty code that won't work well when sourced and this VUE env var guards against running those bits
+  # https://ftp.mirrorservice.org/sites/www.bitsavers.org/pdf/hp/9000_hpux/9.x/B1171-90044_HP_Visual_User_Environment_System_Administration_Manual_Nov91.pdf
+  VUE=true
+  export VUE
+fi
+
+if [ -f /etc/profile ]; then
+  # running on the proxied host or not we want to make sure local customizations are taken
+  # e.g. ent-14014: custom build of ssh needed for build-artifacts-cache needed and /etc/profile has PATH=/opt/craig/bin:$PATH
+  . /etc/profile
+fi
